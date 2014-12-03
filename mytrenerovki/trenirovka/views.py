c2bf172427509b766b9b0eba4c3ecd5fd170292e
StellasVegas <savos@mail.ru>
2014-12-03 14:23:32 +0300

    Доделал модель добавления подходов

--- mytrenerovki/trenirovka/views.py
+++ mytrenerovki/trenirovka/views.py
@@ -39,11 +39,19 @@ def vse_zanyatiya (request) :
 	vse_zanyatiya_list = Zanyatie.objects.all()
 	return render_to_response ('vse_zanyatiya.html', locals() )
 
+@login_required(login_url='/login/')
 def zanyatie (request, item_id=1) :
+	user = request.user.username
 	name_user 		 = Zanyatie.objects.get(id=item_id)
 	yprazneniya_list = Yprazneniya.objects.filter(zanyatie=item_id)
 	zanyatie_list	 = Podhod.objects.filter(zanyatie=item_id)
-	return render_to_response ('zanyatie.html', locals() )
+
+	vid_treni_list = Vid_Treni.objects.all()
+	vid_podhoda_list = Vid_Podhod.objects.all()
+	pp_yprazneniya_list = PP_Yprazneniya.objects.all()
+
+	return render_to_response ('zanyatie.html', locals() , context_instance=RequestContext(request))
+
 
 
 def add_vid_ypr (request) :
@@ -115,29 +123,29 @@ def add_ypraznenie (request) :
 
 
 
-def save_zanyatie (request) :
-	user = request.user.username
-	zanyatie = request.GET['zanyatie']
-	ypraznenie = request.GET['ypraznenie']
-	tip = request.GET['tip']
-	vid = request.GET['vid']
-	podhod_00_ves = request.GET['podhod-1-ves']
-	podhod_00_raz = request.GET['podhod-1-raz']
-	podhod_0_ves = request.GET['podhod-0-ves']
-	podhod_0_raz = request.GET['podhod-0-raz']
-	podhod_1_ves = request.GET['podhod+1-ves']
-	podhod_1_raz = request.GET['podhod+1-raz']
-	podhod_2_ves = request.GET['podhod+2-ves']
-	podhod_2_raz = request.GET['podhod+2-raz']
-	podhod_3_ves = request.GET['podhod+3-ves']
-	podhod_3_raz = request.GET['podhod+3-raz']
-	podhod_4_ves = request.GET['podhod+4-ves']
-	podhod_4_raz = request.GET['podhod+4-raz']
-	podhod_5_ves = request.GET['podhod+5-ves']
-	podhod_5_raz = request.GET['podhod+5-raz']
-
-	z= Zanyatie.objects.get(id=zanyatie)
-	y= Yprazneniya.objects.get(id=ypraznenie)
+#def save_zanyatie (request) :
+#	user = request.user.username
+#	zanyatie = request.GET['zanyatie']
+#	ypraznenie = request.GET['ypraznenie']
+#	tip = request.GET['tip']
+#	vid = request.GET['vid']
+#	podhod_00_ves = request.GET['podhod-1-ves']
+#	podhod_00_raz = request.GET['podhod-1-raz']
+#	podhod_0_ves = request.GET['podhod-0-ves']
+#	podhod_0_raz = request.GET['podhod-0-raz']
+#	podhod_1_ves = request.GET['podhod+1-ves']
+#	podhod_1_raz = request.GET['podhod+1-raz']
+#	podhod_2_ves = request.GET['podhod+2-ves']
+#	podhod_2_raz = request.GET['podhod+2-raz']
+#	podhod_3_ves = request.GET['podhod+3-ves']
+#	podhod_3_raz = request.GET['podhod+3-raz']
+#	podhod_4_ves = request.GET['podhod+4-ves']
+#	podhod_4_raz = request.GET['podhod+4-raz']
+#	podhod_5_ves = request.GET['podhod+5-ves']
+#	podhod_5_raz = request.GET['podhod+5-raz']
+#
+#	z= Zanyatie.objects.get(id=zanyatie)
+#	y= Yprazneniya.objects.get(id=ypraznenie)
 	#pp = PP_Yprazneniya.objects.get(id=ypraznenie)
 	#t= Vid_Treni.objects.get(id=ypraznenie)
 
@@ -145,31 +153,31 @@ def save_zanyatie (request) :
 #P_Podhod, PP_Yprazneniya, Vid_Podhod, Vid_Treni, Podhod, Zanyatie, Yprazneniya
 
 
-	p1=Podhod(zanyatie = z, ypraznenie = y, pp_yprazneniya = u'1', vid_treni = tip, 
-				vid_podhoda = vid, pp_podhoda =  u'1' ,povtoreniya = podhod_00_raz, ves = podhod_00_ves)
+#	p1=Podhod(zanyatie = z, ypraznenie = y, pp_yprazneniya = u'1', vid_treni = tip, 
+#				vid_podhoda = vid, pp_podhoda =  u'1' ,povtoreniya = podhod_00_raz, ves = podhod_00_ves)
 
-	p2=Podhod(zanyatie = z, ypraznenie = y, pp_yprazneniya =  u'1', vid_treni = tip, 
-				vid_podhoda = vid, pp_podhoda =  u'2' ,povtoreniya = podhod_0_raz, ves = podhod_0_ves)
+##	p2=Podhod(zanyatie = z, ypraznenie = y, pp_yprazneniya =  u'1', vid_treni = tip, 
+#				vid_podhoda = vid, pp_podhoda =  u'2' ,povtoreniya = podhod_0_raz, ves = podhod_0_ves)
 
-	p3=Podhod(zanyatie = z, ypraznenie = y, pp_yprazneniya =  u'1', vid_treni = tip, 
-				vid_podhoda = vid, pp_podhoda =  u'3' ,povtoreniya = podhod_1_raz, ves = podhod_1_ves)
+#	p3=Podhod(zanyatie = z, ypraznenie = y, pp_yprazneniya =  u'1', vid_treni = tip, 
+#				vid_podhoda = vid, pp_podhoda =  u'3' ,povtoreniya = podhod_1_raz, ves = podhod_1_ves)
 
-	p4=Podhod(zanyatie = z, ypraznenie = y, pp_yprazneniya =  u'1', vid_treni = tip, 
-				vid_podhoda = vid, pp_podhoda =  u'4' ,povtoreniya = podhod_2_raz, ves = podhod_2_ves)	
+#	p4=Podhod(zanyatie = z, ypraznenie = y, pp_yprazneniya =  u'1', vid_treni = tip, 
+#				vid_podhoda = vid, pp_podhoda =  u'4' ,povtoreniya = podhod_2_raz, ves = podhod_2_ves)	
+#
+#	p5=Podhod( zanyatie = z, ypraznenie = y, pp_yprazneniya =  u'1', vid_treni = tip, 
+#				vid_podhoda = vid, pp_podhoda =  u'5' ,povtoreniya = podhod_3_raz, ves = podhod_3_ves)
+#
+#	p6=Podhod(zanyatie = z, ypraznenie = y, pp_yprazneniya =  u'1', vid_treni = tip, 
+#				vid_podhoda = vid, pp_podhoda =  u'6' ,povtoreniya = podhod_4_raz, ves = podhod_4_ves)
+#
+#	p7=Podhod(zanyatie = z, ypraznenie = y, pp_yprazneniya =  u'1', vid_treni = tip, 
+#				vid_podhoda = vid, pp_podhoda =  u'7' ,povtoreniya = podhod_5_raz, ves = podhod_5_ves)									
 
-	p5=Podhod( zanyatie = z, ypraznenie = y, pp_yprazneniya =  u'1', vid_treni = tip, 
-				vid_podhoda = vid, pp_podhoda =  u'5' ,povtoreniya = podhod_3_raz, ves = podhod_3_ves)
-
-	p6=Podhod(zanyatie = z, ypraznenie = y, pp_yprazneniya =  u'1', vid_treni = tip, 
-				vid_podhoda = vid, pp_podhoda =  u'6' ,povtoreniya = podhod_4_raz, ves = podhod_4_ves)
-
-	p7=Podhod(zanyatie = z, ypraznenie = y, pp_yprazneniya =  u'1', vid_treni = tip, 
-				vid_podhoda = vid, pp_podhoda =  u'7' ,povtoreniya = podhod_5_raz, ves = podhod_5_ves)									
-
-	podhodi_list = [p1,p2,p3,p4,p5,p6,p7]
+#	podhodi_list = [p1,p2,p3,p4,p5,p6,p7]
 
 	#vid_ypr.save()
-	return render_to_response('save_zanyatie.html', locals(), context_instance=RequestContext(request) )
+#	return render_to_response('save_zanyatie.html', locals(), context_instance=RequestContext(request) )
 
 
 def save_zanyatie_ypr (request) :
@@ -217,6 +225,8 @@ def save_zanyatie_ypr (request) :
 
 	podhodi_list = [p1,p2,p3,p4,p5,p6,p7]
 
+	pp_ypr = ypraznenie.pp_yprazneniya.name
+
 	for num, podhod in enumerate(podhodi_list):
 		if podhod is None:
 			ves = 'podhod-%s-ves' % str(num)
@@ -231,7 +241,7 @@ def save_zanyatie_ypr (request) :
 #							vid_podhoda = vid, pp_podhoda =  num ,povtoreniya = povtoreniya, ves = ves)
 
 
-			podhod=Podhod(zanyatie = zanyatie, ypraznenie = ypraznenie, pp_yprazneniya = u'1', vid_treni = tip, 
+			podhod=Podhod(zanyatie = zanyatie, ypraznenie = ypraznenie, pp_yprazneniya = pp_ypr, vid_treni = tip, 
 							vid_podhoda = vid, pp_podhoda =  num )
 			if ves :
 				podhod.ves = ves 
@@ -252,6 +262,7 @@ def save_zanyatie_ypr (request) :
 			if podhod_raz :
 				podhod.povtoreniya = int(podhod_raz)
 			podhod.save()
+			podhodi_list[num] = podhod
 
 
 	#podhodi_list = [p1,p2,p3,p4,p5,p6,p7]
